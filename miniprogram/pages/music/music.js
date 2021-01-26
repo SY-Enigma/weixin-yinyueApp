3203b764572ba19ba39aed444b597d53c73b7464
SY-Enigma <1797441048@qq.com>
2021-01-26 21:50:46 +0800

    图片轮播，自定义组件，歌单组件

--- miniprogram/pages/music/music.js
+++ miniprogram/pages/music/music.js
@@ -5,6 +5,58 @@ Page({
    * 页面的初始数据
    */
   data: {
+    imgUrls:[{
+      url:'http://p1.music.126.net/pOXTFta-mhTpZOGhBBWvhQ==/109951165664682857.jpg?imageView&quality=89'
+ },{
+   url:'http://p1.music.126.net/C9I9GxpvRX7nCZyXNBeqOw==/109951165664694558.jpg?imageView&quality=89'
+ },{
+   url:'http://p1.music.126.net/q5rKcBx9Y0V37DsUSaQKXg==/109951165664695730.jpg?imageView&quality=89'
+ },{
+   url:'http://p1.music.126.net/WOoIZuva_umxxzYOvWINLA==/109951165664707565.jpg?imageView&quality=89'
+ },{
+   url:'http://p1.music.126.net/zUv2mRobckK7Tdn2bp9iSA==/109951165664840470.jpg?imageView&quality=89'
+ },{
+   url:'http://p1.music.126.net/UdSM2BmqY_h_t9HAOzb5dQ==/109951165664710664.jpg?imageView&quality=89'
+ },{
+   url:'http://p1.music.126.net/9Ayx-EeCnuLRWKTcIhGB6g==/109951165664742856.jpg?imageView&quality=89'
+ },{
+   url:'http://p1.music.126.net/j0gp3gBDRRoqIXxAs0v7oA==/109951165664720877.jpg?imageView&quality=89'
+ },{
+   url:'http://p1.music.126.net/Z90NF2dHuBYrV6x-U9jJJQ==/109951165664719544.jpg?imageView&quality=89'
+ }
+ ],
+ playlist:[{
+  "id":"1001",
+  "playCount":"150000000",
+  "name":"IU翻唱的中文歌 宠粉狂魔❤️",
+  "picUrl":"https://sy-enigma.oss-cn-shanghai.aliyuncs.com/iu/IMG_0784.JPG"
+},{
+  "id":"1002",
+  "playCount":"1570",
+  "name":"「IU李知恩」治愈嗓音，听觉盛宴",
+  "picUrl":"https://sy-enigma.oss-cn-shanghai.aliyuncs.com/iu/IMG_0785.JPG"
+},{
+  "id":"1003",
+  "playCount":"106770",
+  "name":"甜美治愈♩李知恩iu",
+  "picUrl":"http://p4.music.126.net/xS_LTZN2wMP_08AaiY-4ZA==/109951164571979631.jpg?param=200y200"
+},{
+  "id":"1004",
+  "playCount":"899",
+  "name":"甜甜的歌单/“你是心动的具象化”",
+  "picUrl":"http://p3.music.126.net/4scEhs5h_4UzDQli201U2w==/109951165210972742.jpg?param=200y200"
+},{
+  "id":"1005",
+  "playCount":"142332",
+  "name":"IU丨清新音色的温柔",
+  "picUrl":"https://sy-enigma.oss-cn-shanghai.aliyuncs.com/iu/IMG_0788.JPG"
+},{
+  "id":"1006",
+  "playCount":"110000",
+  "name":"IU｜IU的宅家Signal",
+  "picUrl":"https://sy-enigma.oss-cn-shanghai.aliyuncs.com/iu/IMG_1008.JPG"
+}
+]
 
   },
 
